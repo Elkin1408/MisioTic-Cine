d1794cc296c6f3fc5032e463b676dc773488c894
sasilgado <sasilgado@uninorte.edu.co>
2021-10-24 17:41:00 -0500

    ultimo edicion

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from werkzeug.utils import secure_filename
 from db import get_db
+import datetime
+import functools
 from wtforms import Form, BooleanField, StringField, PasswordField, SelectField, validators
 from wtforms.fields.simple import SubmitField
 from wtforms.validators import Required
@@ -19,6 +21,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 Bootstrap(app)
 
 app.secret_key = 'hWdsd39lg'
+
+
 
 
 
@@ -48,10 +52,40 @@ class RegisterForm(FlaskForm):
 
 # --------------------------- ROUTES --------------------------- #
 
+def login_required(view):
+    @functools.wraps( view ) # toma una función utilizada en un decorador y añadir la funcionalidad de copiar el nombre de la función.
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect( url_for( 'login' ) ) # si no tiene datos, lo envío a que se loguee
+        return view( **kwargs )
+    return wrapped_view
+
+def admin_required(view):
+    @functools.wraps( view ) # toma una función utilizada en un decorador y añadir la funcionalidad de copiar el nombre de la función.
+    def wrapped_view(**kwargs):
+        if g.user[6]!=1 and g.user[6]!=2:
+            return redirect( url_for( 'home' ) ) # si no tiene datos, lo envío a que se loguee
+        return view( **kwargs )
+    return wrapped_view
+
+def super_required(view):
+    @functools.wraps( view ) # toma una función utilizada en un decorador y añadir la funcionalidad de copiar el nombre de la función.
+    def wrapped_view(**kwargs):
+        if g.user[6]!=1:
+            return redirect( url_for( 'home' ) ) # si no tiene datos, lo envío a que se loguee
+        return view( **kwargs )
+    return wrapped_view
+
 #INDEX
 @app.route('/')
 def home():
-    return render_template('index.html')
+    get_db()
+    db=get_db()
+    result=db.execute("SELECT * FROM pelicula LIMIT 6")
+    result= result.fetchall()
+    db.close()
+    
+    return render_template('index.html',result=result)
 
 #LOGIN
 @app.route('/login/', methods=['POST', 'GET'])
@@ -103,8 +137,10 @@ def login():
                     response.set_cookie( 'username', email  ) # nombre de la cookie y su valor
                     
                     return response
-
-        return redirect(url_for('home'))
+        if g.user[6]==1 or g.user==2:
+            return redirect(url_for('manage_movies'))
+        else:
+            return redirect(url_for('home'))
     
     return render_template('login.html', form=login_form)
 
@@ -117,7 +153,7 @@ def cargar_usuario_registrado():
         g.user = None
     else:
         g.user = get_db().execute(
-                'SELECT id, nombre, email, password FROM usuario WHERE id = ?'
+                'SELECT id, nombre, email, password,fecha_nacimiento,genero,cargo_id FROM usuario WHERE id = ?'
                 ,
                 (id_usuario,)
             ).fetchone()
@@ -190,9 +226,38 @@ def logout():
 
 
 #PROFILE_USER
-@app.route('/perfil/')
+@app.route('/perfil/', methods=['POST','GET'])
 def show_user_profile():
-    return render_template('perfil_user.html')
+    if request.method == 'POST':
+        db=get_db()
+        id = request.form['id']
+        name = request.form['name']
+        email = request.form['email']
+        genero = request.form['genero']
+        date = request.form['date']
+        ## Buscar correo y verificar si ya existe    
+        user_email = db.execute(
+            'SELECT * FROM usuario WHERE email = ?'
+            ,
+            (email,)
+        ).fetchone()            
+        if user_email is not None and g.user[0]!=user_email[0]:
+            error = "Correo ingresado ya existe."
+            flash(error)
+            return redirect (url_for('show_user_profile'))
+        else:
+            #Seguro:
+            sql="UPDATE usuario SET nombre='{}',email='{}',fecha_nacimiento='{}',genero='{}' WHERE id= '{}'".format(name,email,date,genero,id)
+            db.execute(sql)
+            db.commit()
+            flash('Datos actualizados')
+            
+            return redirect (url_for('show_user_profile'))
+            
+    if g.user[6]==1 or g.user[6]==2:
+            return redirect(url_for('manage_movies'))
+    else:
+        return render_template('perfil_user.html')
 
 #BUSCAR
 @app.route('/buscar/<string:movie_name>', methods=['GET'])
@@ -201,8 +266,15 @@ def search(movie_name=None):
 
 #MOVIE DETAILS
 @app.route('/watch/<string:movie>')
-def show_movie(movie=None):
-    return render_template('movie_detailed.html', movie=movie)
+def show_movie(movie):
+    db=get_db()
+    codigo=comment=movie    
+    movie=db.execute("SELECT * FROM pelicula WHERE codigo=?",(movie,))
+    movie= movie.fetchall()
+    comment=db.execute("SELECT contenido,fecha,nombre FROM comentario inner join usuario ON usuario.id=comentario.usuario_id  where pelicula_codigo=?",(comment,))
+    comment= comment.fetchall()
+    db.close()
+    return render_template('movie_detailed.html', movie=movie,comment=comment,pelicula=codigo)
 
 #PRONTO
 @app.route('/pronto/', methods=['GET'])
@@ -210,8 +282,11 @@ def upcoming_movies():
     return render_template('pronto.html')
 
 #FUNCIONES ADMIN
+
 @app.route('/admin_funciones/', methods=['GET', 'POST'])
 def manage_functions():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'GET':
         db=get_db()
         result=db.execute("SELECT * FROM funcion")
@@ -221,8 +296,11 @@ def manage_functions():
     return render_template('funciones.html',result=result)
 
 #PELICULAS ADMIN
+
 @app.route('/admin_peliculas/', methods=['GET', 'POST'])
 def manage_movies():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'GET':
         db=get_db()
         result=db.execute("SELECT * FROM pelicula")
@@ -232,8 +310,11 @@ def manage_movies():
         return render_template('peliculas.html',result=result)
 
 #ROL Y USER ADMIN
+
 @app.route('/admin_user/', methods=['GET', 'POST'])
 def manage_users():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     try:
         if request.method == 'GET':
             db=get_db()
@@ -246,8 +327,11 @@ def manage_users():
         return render_template('rolyuser.html')
 
 #crear peliculas        
+
 @app.route('/admin_peliculas/crear/', methods=['GET', 'POST'])
 def create_movies():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'POST':
         pass
     return render_template('createmovies.html')
@@ -257,10 +341,12 @@ def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-           
+
+          
 @app.route('/admin_peliculas/save/', methods=['GET', 'POST'])
 def save_movies():
-
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'POST':   
         nombre = request.form['titulo']
         genero = request.form['genero']            
@@ -294,6 +380,8 @@ def save_movies():
 
 @app.route('/admin_peliculas/ver/', methods=['GET', 'POST'])
 def seemovies():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'GET':
         
         codigo=request.args.get('codigo')
@@ -303,8 +391,11 @@ def seemovies():
         db.close()
         return render_template('seemovies.html',result=result)
 
+
 @app.route('/admin_peliculas/edit/', methods=['GET', 'POST'])
 def editmovies():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'GET':
         codigo=request.args.get('codigo')
         db=get_db()
@@ -313,8 +404,11 @@ def editmovies():
         db.close()
         return render_template('editmovies.html',result=result)
 
+
 @app.route('/admin_peliculas/update/', methods=['GET', 'POST','PUT'])
 def updatemovies():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'POST':
         nombre = request.form['nombre']
         genero = request.form['genero']            
@@ -325,10 +419,7 @@ def updatemovies():
         cast = request.form['cast']
         idioma = request.form['idioma']
         codigo = request.form['codigo']
-        
 
-        
-        
     db=get_db()
     #"UPDATE Producto  SET nombre = '{}', precio = '{}', existencia = '{}' WHERE id = '{}'".format(nombre, precio, existencia, id)
     sql="UPDATE pelicula SET nombre='{}', descripcion= '{}', genero= '{}', clasificacion= '{}', duracion= '{}', director= '{}' ,cast= '{}', idioma= '{}' WHERE codigo= '{}'".format(nombre,descrip,genero,clasificacion,duracion,director,cast,idioma,codigo)
@@ -340,6 +431,8 @@ def updatemovies():
 
 @app.route('/admin_peliculas/delete/', methods=['GET', 'POST'])
 def deletemovies():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'GET':
         codigo=request.args.get('codigo')
         db=get_db()
@@ -348,9 +441,12 @@ def deletemovies():
         db.close()
         return redirect(url_for( 'manage_movies')) 
 
-#crear funcion        
+#crear funcion    
+ 
 @app.route('/admin_funciones/crear/', methods=['GET', 'POST'])
 def create_funcion():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'GET':
         db=get_db()
         result=db.execute("SELECT codigo,nombre FROM pelicula")            
@@ -359,9 +455,11 @@ def create_funcion():
         
     return render_template('createfuncion.html',result=result)
 
+
 @app.route('/admin_funciones/save/', methods=['GET', 'POST'])
 def save_funcion():
-
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'POST':   
         categoria = request.form['categoria']
         sala = request.form['sala']            
@@ -378,6 +476,7 @@ def save_funcion():
         db.close()  
         return redirect(url_for( 'manage_functions'))
 
+
 @app.route('/admin_funciones/edit/', methods=['GET', 'POST'])
 def editfuncion():
     if request.method == 'GET':
@@ -388,8 +487,11 @@ def editfuncion():
         db.close()
         return render_template('editfuncion.html',result=result)
 
+
 @app.route('/admin_funciones/update/', methods=['GET', 'POST','PUT'])
 def updatefuncion():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'POST':
         id = request.form['id']
         categoria = request.form['categoria']
@@ -397,10 +499,7 @@ def updatefuncion():
         hora = request.form['hora']
         precio = request.form['precio']
         pelicula_codigo = request.form['pelicula_codigo']
-        
-
-        
-        
+  
     db=get_db()
     #"UPDATE Producto  SET ca = '{}', precio = '{}', existencia = '{}' WHERE id = '{}'".format(nombre, precio, existencia, id)
     sql="UPDATE funcion SET categoria='{}', sala= '{}', hora= '{}', precio= '{}', pelicula_codigo= '{}' WHERE id= '{}'".format(categoria,sala,hora,precio,pelicula_codigo,id)
@@ -408,6 +507,7 @@ def updatefuncion():
     db.commit()
     db.close()
     return redirect(url_for( 'manage_functions'))
+
 
 @app.route('/admin_funciones/ver/', methods=['GET', 'POST'])
 def seefuncion():
@@ -419,9 +519,12 @@ def seefuncion():
         result= result.fetchall()
         db.close()
         return render_template('seefuncion.html',result=result)
-    
+
+
 @app.route('/admin_funciones/delete/', methods=['GET', 'POST'])
 def deletefuncion():
+    if g.user[6]!=1 and g.user[6]!=2:
+        return redirect(url_for('home'))
     if request.method == 'GET':
         id=request.args.get('id')
         db=get_db()
@@ -429,3 +532,54 @@ def deletefuncion():
         db.commit()
         db.close()
         return redirect(url_for( 'manage_functions')) 
+
+@login_required
+@app.route('/watch/comment', methods=['GET', 'POST'])
+def createcomment():
+    x = datetime.datetime.now()
+
+    day = x.strftime("%d")
+    month = x.strftime("%b")
+    year = x.strftime("%Y")
+    hour = x.strftime("%X")
+
+    date = f"{day}-{month}-{year} {hour}"
+    if request.method == 'POST':
+        user=request.form['user']
+        comment=request.form['comment']
+        pelicula=request.form['pelicula']
+        if g.user:
+            db=get_db()
+            db.execute("INSERT INTO comentario (contenido,fecha,pelicula_codigo,usuario_id) VALUES (?,?,?,?) "
+                        , (comment,date,pelicula,user) )
+            db.commit()
+            db.close()
+        return redirect(url_for( 'show_movie',movie=pelicula))
+
+@app.route('/perfil/mycomments', methods=['GET', 'POST'])
+def mycomments():
+    get_db()
+    db=get_db()   
+    comment=db.execute("SELECT idcomentario,contenido,fecha,nombre FROM comentario inner join usuario ON usuario.id=comentario.usuario_id  where usuario_id=?",(g.user[0],))
+    comment= comment.fetchall()
+
+    if request.method == 'GET':
+        id=request.args.get('id')
+        modo=request.args.get('modo')
+        if modo=='d':
+            db.execute("DELETE FROM comentario WHERE idcomentario=?",(id,))
+            db.commit()
+            return redirect (url_for('mycomments'))
+
+    
+    
+    if request.method == 'POST':
+        comment = request.form['ecomment']
+        idcomentario = request.form['idcomentario']
+        sql="UPDATE comentario SET contenido='{}' WHERE idcomentario= '{}'".format(comment,idcomentario)
+        db.execute(sql)
+        db.commit()
+        return redirect (url_for('mycomments'))
+    db.close()
+    return render_template('mycomment.html',comment=comment)
+
